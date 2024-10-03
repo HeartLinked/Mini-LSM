200c148ae4311efeb561d8da81b44768ab01b9e2
HeartLinked <lfyhl0907@gmail.com>
2024-10-03 14:56:32 +0800

    [week1] 1.5 task1

--- mini-lsm-starter/src/iterators/two_merge_iterator.rs
+++ mini-lsm-starter/src/iterators/two_merge_iterator.rs
@@ -1,6 +1,3 @@
-#![allow(unused_variables)] // TODO(you): remove this lint after implementing this mod
-#![allow(dead_code)] // TODO(you): remove this lint after implementing this mod
-
 use anyhow::Result;
 
 use super::StorageIterator;
@@ -19,7 +16,7 @@ impl<
     > TwoMergeIterator<A, B>
 {
     pub fn create(a: A, b: B) -> Result<Self> {
-        unimplemented!()
+        Ok(TwoMergeIterator { a, b })
     }
 }
 
@@ -30,19 +27,52 @@ impl<
 {
     type KeyType<'a> = A::KeyType<'a>;
 
-    fn key(&self) -> Self::KeyType<'_> {
-        unimplemented!()
+    fn value(&self) -> &[u8] {
+        match (self.a.is_valid(), self.b.is_valid()) {
+            (true, true) => {
+                if self.a.key() <= self.b.key() {
+                    self.a.value()
+                } else {
+                    self.b.value()
+                }
+            }
+            (true, false) => self.a.value(),
+            _ => self.b.value(),
+        }
     }
 
-    fn value(&self) -> &[u8] {
-        unimplemented!()
+    fn key(&self) -> Self::KeyType<'_> {
+        match (self.a.is_valid(), self.b.is_valid()) {
+            (true, true) => {
+                if self.a.key() <= self.b.key() {
+                    self.a.key()
+                } else {
+                    self.b.key()
+                }
+            }
+            (true, false) => self.a.key(),
+            _ => self.b.key(),
+        }
     }
 
     fn is_valid(&self) -> bool {
-        unimplemented!()
+        self.a.is_valid() || self.b.is_valid()
     }
 
     fn next(&mut self) -> Result<()> {
-        unimplemented!()
+        match (self.a.is_valid(), self.b.is_valid()) {
+            (true, true) => {
+                if self.a.key() < self.b.key() {
+                    self.a.next()
+                } else if self.a.key() == self.b.key() {
+                    self.a.next()?;
+                    self.b.next()  // 如果 key 相等，则两个迭代器都要增加
+                } else {
+                    self.b.next()
+                }
+            }
+            (true, false) => self.a.next(),
+            _ => self.b.next(),
+        }
     }
 }
